04172b88e6b4afd621e03dd7edf0ed87b10fe386
Pleonix372 <sasha.miroshnikov372@gmail.com>
2023-08-21 17:38:52 +0300

    js-train-24

--- 9.js
+++ 9.js
@@ -7,6 +7,17 @@ function task9() {
   // Викликаємо функцію fetchWithError
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+  function fetchWithError() {
+    const errorMessage = "Помилка при з'єднанні з сервером";
+    Promise.reject(errorMessage)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  fetchWithError();
 }
 
 // Викликаємо функцію task10
